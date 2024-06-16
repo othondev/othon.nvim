058e141a6a1902bbfab6229316116846c61eebe2
The Programmer <programmer@gmail.com>
2024-06-16 09:23:10 -0400

    Init

--- lua/othon/init.lua
+++ lua/othon/init.lua
@@ -2,6 +2,12 @@ local M = {}
 
 function M.load_mappings()
   	vim.api.nvim_set_keymap("n", "<TAB>", ':lua print("oi oi oi")<CR>', { noremap = true, silent = true })
+  print('all mappings loaded')
 end
 
+vim.schedule(function ()
+  M.load_mappings()
+end)
+
 return M
+
